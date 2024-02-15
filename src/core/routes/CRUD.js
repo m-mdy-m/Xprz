7ae9ee5c1aabb269362bc2a3825a0a437eff0af9
mahdi <mahdimamashli1383@gmail.com>
2024-02-15 11:51:26 +0330

    Refactor CRUD routes with new handler class

--- src/core/routes/CRUD.js
+++ src/core/routes/CRUD.js
@@ -1,15 +1,34 @@
 const start = require("../../shared/app");
 const app = start();
 
-function send(app, url, response) {
-  app.get(url, (req, res, nxt) => {
-    res.send(response);
-  });
+class handler {
+  constructor(app, url) {
+    this.app = app;
+    this.url = url;
+    this.statusCode = 200;
+  }
+  status(code) {
+    this.statusCode = code;
+    return this;
+  }
+  handlerResponse(data, method) {
+    this.app.get(this.url, (req, res) => {
+      res.status(this.statusCode);
+      method.call(res,data)
+    });
+    return this;
+  }
+  send(data) {
+    return this.handleResponse(data, (res) => res.send(data));
+  }
+
+  write(data) {
+    return this.handleResponse(data, (res) => res.write(data).end());
+  }
 }
 
-function get(url, handler) {
-  app.get(url, handler || ((req, res, next) => {
-    
-  }));
+function get(url) {
+  return new handler(app, url);
 }
-get("/");
+
+get("/").write("hdsadasdasd");
