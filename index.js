ed84299c8d9dea8591f191186a011c79c57b9118
mahdi <mahdimamashli1383@gmail.com>
2024-02-16 21:55:02 +0330

    Changed router initialization from a class to a function.

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const { initApp, listen, launch, getExpress } = require("./src/shared/AppManager");
 launch()
-const RouterManager  = require('./src/core/routes/router')
+const CreateRouter  = require('./src/core/routes/router')
 const get = require("./src/core/CRUD/read");
 const {setEjs }= require('./src/utils/templateEngines');
 setEjs('views')
-const router = new RouterManager()
+const router = CreateRouter()
 console.log(router);
 get('/',{send : 'hi'})
--- src/core/routes/router.js
+++ src/core/routes/router.js
@@ -1,18 +1,8 @@
 const { getExpress } = require("../../shared/AppManager");
 
-class RouterManager {
-  constructor() {
-    const express = getExpress();
-    this.router = express.Router();
-    ;
-  }
-  setRouter(router) {
-    this.router = router;
-  }
-
-  getRouter() {
-    return this.router;
-  }
+function CreateRouter(){
+  const express = getExpress();
+  return express.Router();
 }
 
-module.exports = RouterManager
+module.exports = CreateRouter
