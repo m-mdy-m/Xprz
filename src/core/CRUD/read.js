dbc972f033426deed7c6d88d7a8dd95f99439e0b
mahdi <mahdimamashli1383@gmail.com>
2024-02-16 22:33:33 +0330

    Update read.js to use RouteHandler instead of console.log

--- src/core/CRUD/read.js
+++ src/core/CRUD/read.js
@@ -9,12 +9,10 @@ function saveRouter(routerInstance) {
 }
 function get(url, callbackObj) {
   if (router) {
-    console.log("with route");
     handler = new RouteHandler(router, url);
     applyCallbacks(handler, callbackObj);
     return handler;
   } else {
-    console.log("without route");
     const app = getApp();
     handler = new RouteHandler(app, url);
     applyCallbacks(handler, callbackObj);
