798e36a060679ae76e8ba1163687efc6d681dd22
mahdi <mahdimamashli1383@gmail.com>
2024-02-16 12:47:36 +0330

    Remove template engine setup code.

---
+++ src/utils/templateEngines.js
@@ -0,0 +1,24 @@
+const { getApp } = require("../Using");
+let app = getApp();
+function setEjs(dir) {
+  app.set("view engine", "ejs");
+  app.set("views", dir ? dir : "views");
+}
+function setHBS(hbs, dir, options = {}) {
+  const defaultOptions = {
+    defaultLayout: "main",
+    layoutsDir: (dir, "layouts"),
+    partialsDir: (dir, "partials"),
+    extname: ".hbs",
+  };
+
+  const combinedOptions = { ...defaultOptions, ...options };
+
+  app.set("view engine", "hbs");
+  app.set("views", dir);
+  app.engine(".hbs", hbs(combinedOptions));
+}
+function setPug(dir) {
+  app.set("view engine", "pug");
+  app.set("views", dir ? dir : "views");
+}
