b95475d63ba905eab6cce31f469f52535655b059
mahdi <mahdimamashli1383@gmail.com>
2024-02-16 22:41:00 +0330

    Update router implementation

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const { launch } = require("./src/shared/AppManager");
 launch();
-const RouterManager = require("./src/core/routes/router");
-const { get, saveRouter } = require("./src/core/CRUD/read");
+const Route = require("./src/core/routes/router");
+const { get, setRoute } = require("./src/core/CRUD/read");
 const { setEjs } = require("./src/utils/templateEngines");
 
 setEjs("views");
-const router = new RouterManager();
-saveRouter(router);
+const router = new Route();
+setRoute(router);
 get("/", { send: "hi" });
--- src/core/CRUD/read.js
+++ src/core/CRUD/read.js
@@ -3,19 +3,17 @@ const RouteHandler = require("../../handler/RouteHandler");
 const { applyCallbacks } = require("../../utils/callbackHandler");
 let handler,router
 
-function saveRouter(routerInstance) {
-  router = routerInstance;
+function setRoute(r) {
+  router = r;
 }
 function get(url, callbackObj) {
   if (router) {
     handler = new RouteHandler(router, url);
-    applyCallbacks(handler, callbackObj);
-    return handler;
   } else {
     const app = getApp();
     handler = new RouteHandler(app, url);
-    applyCallbacks(handler, callbackObj);
-    return handler;
   }
+  applyCallbacks(handler, callbackObj);
+  return handler;
 }
-module.exports = { get, saveRouter };
+module.exports = { get, setRoute };
