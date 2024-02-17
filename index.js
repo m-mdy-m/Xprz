e023e86078585a2c1c52cdb439d85f4a0630065e
mahdi <mahdimamashli1383@gmail.com>
2024-02-17 12:50:18 +0330

    Refactor code to use functions instead of variables.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const { launch } = require("./src/shared/AppManager");
 launch();
 const Route = require("./src/core/routes/router");
-const { getApp } = require("./src/Using");
 const { get, setRoute } = require("./src/core/CRUD/read");
 const { setEjs } = require("./src/utils/templateEngines");
-const { use } = require("./src/utils/appUtils");
+const { use } = require("./src/utils/funcs");
 
 setEjs("views");
 const router = new Route();
-use('/',router)
+use(router)
 setRoute(router);
 get("/", { send: "hi" });
