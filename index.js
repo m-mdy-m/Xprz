d29829ddd9679a12f6c941e3c49f30f089207858
mahdi <mahdimamashli1383@gmail.com>
2024-02-17 12:57:50 +0330

    change file name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { launch } = require("./src/shared/AppManager");
 launch();
 const Route = require("./src/core/routes/router");
-const { get, setRoute } = require("./src/core/CRUD/read");
+const { get, setRoute } = require("./src/core/CRUD/GET");
 const { setEjs } = require("./src/utils/templateEngines");
 const { use } = require("./src/utils/funcs");
 
