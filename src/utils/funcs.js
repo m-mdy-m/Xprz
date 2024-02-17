bf14a8b575efc1f30c8265290c8ad77b59a77cc8
mahdi <mahdimamashli1383@gmail.com>
2024-02-17 12:51:24 +0330

    add funcs utils

---
+++ src/utils/funcs.js
@@ -0,0 +1,11 @@
+const { getApp } = require('../Using');
+const app = getApp();
+
+
+exports.set = function(...handler){
+    return app.set(...handler);
+
+}
+exports.use=function(...handler){
+    return app.use(...handler);
+}
