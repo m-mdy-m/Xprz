f2463abb0bb98c0cc36ea9dcc2bf57f65acd8eba
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 11:58:35 +0330

    Update route manager and add middleware

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 const { launch } = require("./src/shared/AppManager");
-launch()
+launch();
 const { getApp } = require("./src/Using");
-const Route = require('./src/shared/RouteManager')
-const app = getApp()
-const router  = new Route()
-// router.setRoute('/').get((req,res)=>{
-//     res.send('hi')
-// }).attachTo(app)
-
-router.group('/api',(route)=>{
-    route.setRoute('/test').get((req,res,nxt)=>{
-        res.send('hi this is test')
-    })
-}).attachTo(app)
+const Route = require("./src/shared/RouteManager");
+const app = getApp();
+const router = new Route();
+function middleware(req, res, next) {
+    console.log("hi");
+    next(); 
+}
+router
+  .setRoute("/")
+  .get((req, res, nxt) => {
+    res.send("hi");
+  });
--- src/shared/RouteManager.js
+++ src/shared/RouteManager.js
@@ -1,134 +1,48 @@
 const { getExpress } = require("./AppManager");
 
-/**
- * Represents a route manager for defining routes using Express.
- */
+
 class Route {
-  /**
-   * Creates an instance of Route.
-   */
   constructor() {
     const express = getExpress();
-    // Create a new router instance from Express
     this.router = express.Router();
+    this.middleware = []
   }
-
-  /**
-   * Sets the route path for the current route.
-   *
-   * @param {string} path - The route path.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.setRoute("/users");
-   */
+  use(middleware){
+    this.middleware.push(middleware)
+    return this
+  }
   setRoute(path) {
     this.path = path;
     return this;
   }
-  /**
-   * Groups related routes under a common route prefix.
-   *
-   * @param {string} mainRoute - The main route prefix.
-   * @param {Function} callback - A callback function to define routes within the group.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.group('/api', (r) => {
-   *   r.setRoute('/users').get((req, res) => {
-   *     res.send('GET request to /api/users');
-   *   });
-   * }).attachTo(app);
-   */
+
   group(mainRoute, callback) {
     const r = new Route();
     callback(r);
     this.router.use(mainRoute, r.router);
     return this;
   }
-  /**
-   * Defines a GET route.
-   *
-   * @param {...Function} handler - The route handler functions.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.setRoute("/users").get((req, res) => {
-   *   res.send("GET request to /users");
-   * });
-   */
+
   get(...handler) {
     this.router.get(this.path, ...handler);
     return this;
   }
 
-  /**
-   * Defines a POST route.
-   *
-   * @param {...Function} handler - The route handler functions.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.setRoute("/users").post((req, res) => {
-   *   res.send("POST request to /users");
-   * });
-   */
   post(...handler) {
     this.router.post(this.path, ...handler);
     return this;
   }
 
-  /**
-   * Defines a DELETE route.
-   *
-   * @param {...Function} handler - The route handler functions.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.setRoute("/users").del((req, res) => {
-   *   res.send("DELETE request to /users");
-   * });
-   */
   del(...handler) {
     this.router.delete(this.path, ...handler);
     return this;
   }
 
-  /**
-   * Defines a PUT route.
-   *
-   * @param {...Function} handler - The route handler functions.
-   * @returns {Route} The Route instance for method chaining.
-   *
-   * @example
-   * const route = new Route();
-   * route.setRoute("/users").put((req, res) => {
-   *   res.send("PUT request to /users");
-   * });
-   */
   put(...handler) {
     this.router.put(this.path, ...handler);
     return this;
   }
-
-  /**
-   * Attaches the route to the specified Express application.
-   *
-   * @param {Object} app - The Express application instance.
-   * @returns {void}
-   *
-   * @example
-   * const app = getApp()
-   * const route = new Route();
-   * route.setRoute("/users").get((req, res) => {
-   *   res.send("GET request to /users");
-   * }).attachTo(app);
-   */
+  
   attachTo(app) {
     app.use(this.router);
   }
