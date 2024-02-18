a5d56e1d8ebcee4e711aac8835cfd17a4ba9c0fe
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 20:45:25 +0330

    add jsDoc for bcryptjs Handler

--- src/handler/package/bcryptjs.js
+++ src/handler/package/bcryptjs.js
@@ -1,18 +1,53 @@
-class bcryptjsHandler {
+/**
+ * Wrapper class for bcryptjs functions.
+ */
+class BcryptjsHandler {
+  /**
+   * Create a new instance of BcryptjsHandler.
+   * @param {Object} bcryptjs - The bcryptjs module.
+   */
   constructor(bcryptjs) {
     /** @private */
     this.bcryptjs = bcryptjs;
   }
+
+  /**
+   * Get the bcryptjs module.
+   * @returns {Object} The bcryptjs module.
+   */
   getBcryptjs() {
     return this.bcryptjs;
   }
-  async has(password, range) {
-    const has = await this.bcryptjs.has(password, range);
-    return has;
+
+  /**
+   * Hashes a password.
+   * @param {string} password - The password to hash.
+   * @param {number} [saltRounds=10] - The number of salt rounds to use (default is 10).
+   * @returns {string} The hashed password.
+   * @example
+   * const bcrypt = require('bcryptjs');
+   * const bcryptHandler = new BcryptjsHandler(bcrypt);
+   * const hashedPassword = await bcryptHandler.hash('myPassword');
+   */
+  async hash(password, saltRounds = 10) {
+    const hashedPassword = await this.bcryptjs.hash(password, saltRounds);
+    return hashedPassword;
   }
-  async compare(basePassword, password) {
-    const isMatch = await this.bcryptjs.compare(basePassword, password);
+
+  /**
+   * Compares a password with a hashed password to check for a match.
+   * @param {string} basePassword - The original password.
+   * @param {string} hashedPassword - The hashed password to compare against.
+   * @returns {boolean} True if the passwords match, false otherwise.
+   * @example
+   * const bcrypt = require('bcryptjs');
+   * const bcryptHandler = new BcryptjsHandler(bcrypt);
+   * const isMatch = await bcryptHandler.compare('myPassword', hashedPassword);
+   */
+  async compare(basePassword, hashedPassword) {
+    const isMatch = await this.bcryptjs.compare(basePassword, hashedPassword);
     return isMatch;
   }
 }
-module.exports = bcryptjsHandler;
+
+module.exports = BcryptjsHandler;
