b06372e6bf44735c3ace6f7f328e598ac4133540
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 20:53:54 +0330

    add jsDoc jwt

--- src/handler/package/jwt.js
+++ src/handler/package/jwt.js
@@ -1,11 +1,33 @@
+/**
+ * jwtHandler class for handling JSON Web Tokens (JWT) in an Express application.
+ */
 class jwtHandler {
+  /**
+   * Creates an instance of jwtHandler.
+   * @param {Object} jwt - The JSON Web Token package.
+   */
   constructor(jwt) {
     /** @private */
     this.jwt = jwt;
   }
+
+  /**
+   * Get the JSON Web Token package.
+   * @returns {Object} The JSON Web Token package.
+   */
   getJwt() {
     return this.jwt;
   }
+
+  /**
+   * Sign a JWT token with the provided payload and secret key.
+   * @param {Object} payload - The payload to be signed into the JWT.
+   * @param {string} secretKey - The secret key used for signing the JWT.
+   * @param {Object} [options={}] - Additional options for signing the JWT.
+   * @returns {string} The signed JWT token.
+   * @example
+   * const token = jwtHandler.jwtSign({ userId: '123' }, 'secret');
+   */
   jwtSign(payload, secretKey, options = {}) {
     if (!payload || !secretKey) {
       throw new Error("Payload and secret key are required for JWT signing.");
@@ -14,11 +36,18 @@ class jwtHandler {
     return this.jwt.sign(payload, secretKey, options);
   }
 
+  /**
+   * Verify a JWT token with the provided secret key.
+   * @param {string} token - The JWT token to be verified.
+   * @param {string} secretKey - The secret key used for verifying the JWT.
+   * @returns {Object} The decoded payload if the token is valid.
+   * @throws {Error} If the token or secret key is missing, or if the token verification fails.
+   * @example
+   * const decoded = jwtHandler.jwtVerify(token, 'secret');
+   */
   jwtVerify(token, secretKey) {
     if (!token || !secretKey) {
-      throw new Error(
-        "Token and secret key are required for JWT verification."
-      );
+      throw new Error("Token and secret key are required for JWT verification.");
     }
 
     try {
@@ -27,6 +56,14 @@ class jwtHandler {
       throw new Error("Failed to verify JWT token.");
     }
   }
+
+  /**
+   * Check if a JWT token is expired.
+   * @param {string} token - The JWT token to be checked.
+   * @returns {boolean} True if the token is expired, false otherwise.
+   * @example
+   * const isExpired = jwtHandler.isTokenExpired(token);
+   */
   isTokenExpired(token) {
     try {
       const decoded = this.jwt.decode(token);
@@ -37,4 +74,5 @@ class jwtHandler {
     }
   }
 }
+
 module.exports = jwtHandler;
