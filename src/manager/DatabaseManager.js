09ac46f354a05e053865a199eb1c44301edbb124
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 20:24:20 +0330

    Added MongoDB and MySQL database support to the application manager.

--- src/manager/DatabaseManager.js
+++ src/manager/DatabaseManager.js
@@ -1,12 +1,42 @@
 const AppManager = require("./AppManager");
-class DataBaseManager extends AppManager{
+const MongoDb = require('../handler/database/MongoDB'), MySql = require('../handler/database/MySql')
+function checkPkg(packageName) {
+    try {
+      const requiredPackage = require(packageName);
+      return requiredPackage;
+    } catch {
+      throw new Error(
+        `The '${packageName}' module is not installed. Please make sure to install it by running 'npm install ${packageName}' before using sessions.`
+      );
+    }
+  }
+  class DataBaseManager extends AppManager {
     constructor() {
-        super()
+        super();
     }
-    MySql(){
 
+    /**
+     * Create and return a new instance of MySql.
+     * @returns {MySql} A new instance of MySql.
+     * @example
+     * const dbManager = new DataBaseManager();
+     * const mysql = dbManager.MySql();
+     */
+    MySql() {
+        const pkg = checkPkg("mysql" || "mysql2");
+        return new MySql(pkg);
     }
-    MongoDb(){
-        
+
+    /**
+     * Create and return a new instance of MongoDb.
+     * @returns {MongoDb} A new instance of MongoDb.
+     * @example
+     * const dbManager = new DataBaseManager();
+     * const mongodb = dbManager.MongoDb();
+     */
+    MongoDb() {
+        const pkg = checkPkg('mongodb');
+        return new MongoDb(pkg);
     }
 }
+module.exports = DataBaseManager
