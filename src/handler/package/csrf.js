4baca2729f825e7f96fa02b2d4c5410576949d1e
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 20:50:43 +0330

    add csrf jsDoc

--- src/handler/package/csrf.js
+++ src/handler/package/csrf.js
@@ -1,19 +1,39 @@
+/**
+ * CsrfHandler class for configuring CSRF protection middleware in an Express application.
+ */
 class CsrfHandler {
+  /**
+   * Creates an instance of CsrfHandler.
+   * @param {Function} csrf - The CSRF package.
+   * @param {Function} use - The Express app's `use` function.
+   */
   constructor(csrf, use) {
-    // /** @private */
+    /** @private */
     this.csrf = csrf;
-    // /** @private */
+    /** @private */
     this.use = use;
-    // /** @private */
-    this.Protection = this.csrf();
-    // /** @private☻ */
-    this.use(this.Protection);
+    /** @private */
+    this.protection = this.csrf();
+    /** @private */
+    this.use(this.protection);
   }
+
+  /**
+   * Get the configured CSRF middleware.
+   * @returns {Function} The configured CSRF middleware.
+   */
   get() {
     return this.csrf;
   }
+
+  /**
+   * Configure CSRF protection middleware with custom options.
+   * @param {Object} options - Options for configuring CSRF protection.
+   * @returns {Function} The configured CSRF protection middleware.
+   */
   configure(options) {
     return this.csrf(options);
   }
 }
-module.exports = CsrfHandler
+
+module.exports = CsrfHandler;
