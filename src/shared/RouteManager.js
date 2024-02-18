90034bfef72ffaac4f7e93c845cb511293295276
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 12:40:35 +0330

    add jsDoc and comments

--- src/shared/RouteManager.js
+++ src/shared/RouteManager.js
@@ -1,64 +1,130 @@
 const { getExpress } = require("./AppManager");
-let middleware = [],
-  has;
 
-function isMiddleware() {
-  return middleware.length > 0 ? true : false;
-}
-
-function registerRoute(method, route, path, ...handler) {
-  const routeHandlers = [...middleware, ...handler];
-  route[method](path, routeHandlers);
-  return;
-}
-class Route {
+/**
+ * RouteManager class handles route management for Express.js.
+ */
+class RouteManager {
+  /**
+   * Creates a new instance of the RouteManager class.
+   */
   constructor() {
-    const express = getExpress();
-    this.router = express.Router();
+    /** @private */
+    this.router = getExpress().Router();
+    /** @private */
+    this.middleware = [];
   }
-  use(m) {
-    middleware.push(m);
-    has = isMiddleware();
+
+  /**
+   * Attaches the route manager to an Express app.
+   * @param {Object} app - The Express app instance.
+   */
+  attachTo(app) {
+    app.use(this.router);
+  }
+  /**
+   * Registers middleware for the route manager.
+   * @param {function} middleware - Middleware function.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  use(middleware) {
+    // Add middleware to the list
+    this.middleware.push(middleware);
+    // Check if middleware is present
+    /**
+     * @private
+     */
+    this.hasMiddleware = this.middleware.length > 0 ? true : false;
     return this;
   }
-  setRoute(path) {
+
+  /**
+   * Sets the base path for the route manager.
+   * @param {string} path - Base path for the route manager.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  setBasePath(path) {
+    // Set the base path for the route manager
+    /**
+     * @private
+     */
     this.path = path;
     return this;
   }
 
+  /**
+   * Groups routes under a common path.
+   * @param {string} mainRoute - Main route path.
+   * @param {function} callback - Callback function to define grouped routes.
+   * @returns {RouteManager} The RouteManager instance.
+   */
   group(mainRoute, callback) {
-    const subRoute = new Route();
-    callback(subRoute);
-    this.router.use(mainRoute, subRoute.router);
+    // Create a new RouteManager instance
+    const subRouteManager = new RouteManager();
+    // Define routes within the callback function
+    callback(subRouteManager);
+    // Mount the sub-route manager on the main route
+    this.router.use(mainRoute, subRouteManager.router);
     return this;
   }
 
-  get(...handler) {
-    if (has) {
-      registerRoute("get", this.router, this.path, handler);
+  /**
+   * Registers a route with a GET method.
+   * @param {...function} handlers - Route handler functions.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  get(...handlers) {
+    if (this.hasMiddleware) {
+      // Register route with middleware
+      this.registerRoute("get", handlers);
     }
-    this.router.get(this.path, ...handler);
+    // Register route without middleware
+    this.router.get(this.path, ...handlers);
     return this;
   }
 
-  post(...handler) {
-    this.router.post(this.path, ...handler);
+  /**
+   * Registers a route with a POST method.
+   * @param {...function} handlers - Route handler functions.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  post(...handlers) {
+    // Register POST route
+    this.router.post(this.path, ...handlers);
     return this;
   }
 
-  del(...handler) {
-    this.router.delete(this.path, ...handler);
+  /**
+   * Registers a route with a DELETE method.
+   * @param {...function} handlers - Route handler functions.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  del(...handlers) {
+    // Register DELETE route
+    this.router.delete(this.path, ...handlers);
     return this;
   }
 
-  put(...handler) {
-    this.router.put(this.path, ...handler);
+  /**
+   * Registers a route with a PUT method.
+   * @param {...function} handlers - Route handler functions.
+   * @returns {RouteManager} The RouteManager instance.
+   */
+  put(...handlers) {
+    // Register PUT route
+    this.router.put(this.path, ...handlers);
     return this;
   }
 
-  attachTo(app) {
-    app.use(this.router);
+  /**
+   * Registers a route with the given method, path, and handlers.
+   * @private
+   */
+  registerRoute(method, handlers) {
+    // Combine middleware with route handlers
+    const routeHandlers = [...this.middleware, ...handlers];
+    // Register the route with Express router
+    this.router[method](this.path, routeHandlers);
   }
 }
 
-module.exports = Route;
+module.exports = RouteManager;
