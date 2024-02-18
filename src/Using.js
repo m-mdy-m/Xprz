5fc9a1c840b8e8197def84020a0199e2a15e28dc
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 11:17:45 +0330

    add new comments

--- src/Using.js
+++ src/Using.js
@@ -1,10 +1,29 @@
+/**
+ * Represents a utility class for managing the Express application instance.
+ */
 class Using {
   constructor() {
+    // Initialize properties
     this.appInstance = null;
+    // Method to set the Express application instance
     this.setAppInstance = function (app) {
       this.appInstance = app;
     };
   }
+
+  /**
+   * Gets the current Express application instance.
+   * 
+   * @returns {Object|null} The Express application instance, or null if not initialized.
+   * 
+   * @example
+   * const app = getApp();
+   * if (app) {
+   *   // Use the Express application instance
+   * } else {
+   *   console.log('Express app instance has not been initialized yet.');
+   * }
+   */
   getAppInstance() {
     if (!this.appInstance) {
       console.log("Express app instance has not been initialized yet.");
@@ -12,7 +31,11 @@ class Using {
     return this.appInstance;
   }
 }
+
+// Create an instance of Using
 const using = new Using();
+
+// Export methods bound to the Using instance
 module.exports = {
   setApp: using.setAppInstance.bind(using),
   getApp: using.getAppInstance.bind(using),
