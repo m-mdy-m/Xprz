f237f37dd376826d436399a0360972513470e516
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 17:14:43 +0330

    add jsDoc and change names

--- src/handler/multer.js
+++ src/handler/multer.js
@@ -1,42 +1,84 @@
-class multer {
+/**
+ * Wrapper class for configuring and using Multer middleware.
+ */
+class Multer {
+  /**
+   * Constructor to initialize the Multer instance.
+   * @param {Object} multer - The Multer instance.
+   */
   constructor(multer) {
-    /**@private */
+    /** @private */
     this.multer = multer;
   }
+
   /**
    * Returns the configured Multer instance.
    * @returns {Object} The Multer instance.
    */
-  getMulter() {
+  get() {
     return this.multer;
   }
+
   /**
    * Configures disk storage for file uploads.
    * @param {Object} options - Options for configuring disk storage.
    * @returns {Object} The configured disk storage.
    */
-  DiskStorage(options) {
-    const fileStorage = this.multer.diskStorage(options);
-    return fileStorage;
+  disk(options) {
+    return this.multer.diskStorage(options);
   }
-  fileFilter(...handler) {
-    this.fileFilter = handler;
-    return this.fileFilter;
+
+  /**
+   * Sets the file filter for uploaded files.
+   * @param {Function} filter - The file filter function.
+   */
+  filter(filter) {
+    this.fileFilter = filter;
   }
-  multerWithSingleOfFiles([...options], file) {
-    const multer = this.getMulter();
-    multer(...options).single(file);
+
+  /**
+   * Configures Multer to handle a single file upload.
+   * @param {Array} options - Options for configuring Multer.
+   * @param {string} field - The name of the file field in the form.
+   * @example
+   * multer.single(['image']);
+   */
+  single(options, field) {
+    this.multer(options).single(field);
   }
-  multerWithArrayOfFiles([...options], ...files) {
-    const multer = this.getMulter();
-    multer(...options).array(...files);
+
+  /**
+   * Configures Multer to handle an array of file uploads.
+   * @param {Array} options - Options for configuring Multer.
+   * @param {...string} fields - The names of the file fields in the form.
+   * @example
+   * multer.array(['images']);
+   */
+  array(options, ...fields) {
+    this.multer(options).array(...fields);
   }
-  multerWithFieldsOfFiles([...options], ...files) {
-    const multer = this.getMulter();
-    multer(...options).fields(...files);
+
+  /**
+   * Configures Multer to handle multiple fields of files.
+   * @param {Array} options - Options for configuring Multer.
+   * @param {...string[]} fields - The names of the fields containing files.
+   * @example
+   * multer.fields(['avatar', 'photos']);
+   */
+  fields(options, ...fields) {
+    this.multer(options).fields(...fields);
   }
-  multerWithAnyOfFiles([...options], ...files) {
-    const multer = this.getMulter();
-    multer(...options).any(...files);
+
+  /**
+   * Configures Multer to handle any type of file upload.
+   * @param {Array} options - Options for configuring Multer.
+   * @param {...string} fields - The names of the file fields in the form.
+   * @example
+   * multer.any(['files']);
+   */
+  any(options, ...fields) {
+    this.multer(options).any(...fields);
   }
 }
+
+module.exports = Multer;
