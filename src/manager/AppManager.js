923b08e1f86b536739d10e4f0f5821ab3094c733
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 15:12:54 +0330

    Update AppManager to use express.static instead of custom static method

--- src/manager/AppManager.js
+++ src/manager/AppManager.js
@@ -1,31 +1,21 @@
 const App = require("../shared/App"),
-  { static } = require("../utils/expressUtils")
+  { static } = require("../utils/expressUtils");
+
 class AppManager extends App {
   constructor() {
     super();
   }
-  use(...handler){
-    this.app.use(...handler)
+  use(...handler) {
+    this.app.use(...handler);
   }
-  set(...handler){
-    this.app.set(...handler)
+  set(...handler) {
+    this.app.set(...handler);
   }
-  static(...handlers){
-    const express = this.getExpress()
-    this.use(express.static(...handlers))
+  static(...handlers) {
+    const express = this.getExpress();
+    this.use(express.static(...handlers));
   }
-  Session(...options) {
-    if (!this.app) {
-      throw new Error("Express app has not been initialized yet.");
-    }
-    const session = require("express-session");
-    if (!session) {
-      throw new Error(
-        "The 'express-session' module is not installed. Please make sure to install it by running 'npm install express-session' before using sessions."
-      );
-    }
-    this.app.use(session(...options));
-  }
+  
   async shutdown() {
     return new Promise((resolve, reject) => {
       this.app.close((err) => {
@@ -38,3 +28,4 @@ class AppManager extends App {
     });
   }
 }
+module.exports = AppManager
