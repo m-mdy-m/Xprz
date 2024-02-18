6b919bc4054fb0053412866cf50a49bfe8bd9d87
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 11:13:54 +0330

    add jsDoc  for RouteManager and app utils

--- src/shared/RouteManager.js
+++ src/shared/RouteManager.js
@@ -1,29 +1,115 @@
 const { getExpress } = require("./AppManager");
+
+/**
+ * Represents a route manager for defining routes using Express.
+ */
 class Route {
+  /**
+   * Creates an instance of Route.
+   */
   constructor() {
     const express = getExpress();
+    // Create a new router instance from Express
     this.router = express.Router();
   }
+
+  /**
+   * Sets the route path for the current route.
+   * 
+   * @param {string} path - The route path.
+   * @returns {Route} The Route instance for method chaining.
+   * 
+   * @example
+   * const route = new Route();
+   * route.setRoute("/users");
+   */
   setRoute(path) {
     this.path = path;
     return this;
   }
+
+  /**
+   * Defines a GET route.
+   * 
+   * @param {...Function} handler - The route handler functions.
+   * @returns {Route} The Route instance for method chaining.
+   * 
+   * @example
+   * const route = new Route();
+   * route.setRoute("/users").get((req, res) => {
+   *   res.send("GET request to /users");
+   * });
+   */
   get(...handler) {
     this.router.get(this.path, ...handler);
     return this;
   }
+
+  /**
+   * Defines a POST route.
+   * 
+   * @param {...Function} handler - The route handler functions.
+   * @returns {Route} The Route instance for method chaining.
+   * 
+   * @example
+   * const route = new Route();
+   * route.setRoute("/users").post((req, res) => {
+   *   res.send("POST request to /users");
+   * });
+   */
   post(...handler) {
     this.router.post(this.path, ...handler);
     return this;
   }
+
+  /**
+   * Defines a DELETE route.
+   * 
+   * @param {...Function} handler - The route handler functions.
+   * @returns {Route} The Route instance for method chaining.
+   * 
+   * @example
+   * const route = new Route();
+   * route.setRoute("/users").del((req, res) => {
+   *   res.send("DELETE request to /users");
+   * });
+   */
   del(...handler) {
     this.router.delete(this.path, ...handler);
     return this;
   }
+
+  /**
+   * Defines a PUT route.
+   * 
+   * @param {...Function} handler - The route handler functions.
+   * @returns {Route} The Route instance for method chaining.
+   * 
+   * @example
+   * const route = new Route();
+   * route.setRoute("/users").put((req, res) => {
+   *   res.send("PUT request to /users");
+   * });
+   */
   put(...handler) {
     this.router.put(this.path, ...handler);
     return this;
   }
+
+  /**
+   * Attaches the route to the specified Express application.
+   * 
+   * @param {Object} app - The Express application instance.
+   * @returns {void}
+   * 
+   * @example
+   * const express = require('express');
+   * const app = express();
+   * const route = new Route();
+   * route.setRoute("/users").get((req, res) => {
+   *   res.send("GET request to /users");
+   * }).attachTo(app);
+   */
   attachTo(app) {
     app.use(this.router);
   }
--- src/utils/appUtils.js
+++ src/utils/appUtils.js
@@ -1,11 +1,33 @@
+// Import the Express module
 const express = require('express');
 
+/**
+ * Initializes and returns a new Express application instance.
+ * 
+ * @returns {Object} A new Express application instance.
+ */
 function initApp(){
-    return express()
+    return express();
 }
+
+/**
+ * Returns the Express module.
+ * 
+ * @returns {Object} The Express module.
+ */
 function getExpress(){
-    return express
+    return express;
 }
+
+/**
+ * Starts the Express application on the specified port and logs a message.
+ * 
+ * @param {Object} app - The Express application instance to listen on.
+ * @param {number} [port=3000] - The port number to listen on. Default is 3000.
+ * @param {string} [textLog='Server is running on port ${port}'] - The message to log upon successful start.
+ * @param {boolean} [log=true] - Indicates whether to log the message. Default is true.
+ * @returns {void}
+ */
 function listen(app, port = 3000, textLog = `Server is running on port ${port}`, log = true) {
     app.listen(port, () => {
         if (log) {
@@ -14,6 +36,7 @@ function listen(app, port = 3000, textLog = `Server is running on port ${port}`,
     });
 }
 
+// Export the functions for use in other modules
 module.exports = {
     initApp,
     getExpress,
