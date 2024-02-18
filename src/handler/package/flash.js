50646d9250e61b97ec73b104a6e8b0a21ce00c92
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 20:51:17 +0330

    add jsDoc for flash

--- src/handler/package/flash.js
+++ src/handler/package/flash.js
@@ -1,14 +1,28 @@
+/**
+ * FlashHandler class for configuring flash middleware in an Express application.
+ */
 class FlashHandler {
-  constructor(flash,use) {
+  /**
+   * Creates an instance of FlashHandler.
+   * @param {Function} flash - The flash package.
+   * @param {Function} use - The Express app's `use` function.
+   */
+  constructor(flash, use) {
     /** @private */
     this.flash = flash;
     /** @private */
-    this.use = use
+    this.use = use;
     /** @private */
-    this.use(this.flash())
+    this.use(this.flash());
   }
+
+  /**
+   * Get the configured flash middleware.
+   * @returns {Function} The configured flash middleware.
+   */
   get() {
     return this.flash;
   }
 }
-module.exports = FlashHandler
+
+module.exports = FlashHandler;
