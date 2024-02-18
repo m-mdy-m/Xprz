688df7734fa1168e8179f978a52a86ebe66a17f6
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 19:20:33 +0330

    Refactor MySql.js to use new query method

--- src/handler/database/MySql.js
+++ src/handler/database/MySql.js
@@ -2,19 +2,12 @@ class MySql {
   constructor(pkg) {
     this.mysql = pkg;
   }
-  connect(
-    host,
-    user,
-    password,
-    database,
-    log = true,
-    textLog = "MySqlConnected"
-  ) {
+  connect(db, log = true, textLog = "MySqlConnected") {
     this.connection = this.mysql({
-      host: host,
-      user: user,
-      password: password,
-      database: database,
+      host: db.host,
+      user: db.user,
+      password: db.password,
+      database: db.database,
     });
     this.connection.connect((err) => {
       if (err) {
@@ -25,6 +18,18 @@ class MySql {
       }
     });
   }
+  /** @private */
+  async query(sql, values) {
+    return new Promise((resolve, reject) => {
+      this.connection.query(sql, values, (err, results) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        resolve(results);
+      });
+    });
+  }
   endConnect(log = true, textLog = "MySQL connection ended") {
     if (this.connection) {
       this.connection.end();
@@ -32,6 +37,29 @@ class MySql {
         console.log(textLog);
       }
     }
-    throw new Error('No MySql Connection')
+    throw new Error("No MySql Connection");
+  }
+  async create(tableName, values) {
+    const sql = `INSERT INTO ${tableName} SET ?`;
+    const result = await this.query(sql, values);
+    return result;
+  }
+
+  async read(tableName, condition) {
+    const sql = `SELECT * FROM ${tableName} WHERE ?`;
+    const result = await this.query(sql, condition);
+    return result;
+  }
+
+  async update(tableName, values, condition) {
+    const sql = `UPDATE ${tableName} SET ? WHERE ?`;
+    const result = await this.query(sql, [values, condition]);
+    return result;
+  }
+
+  async delete(tableName, condition) {
+    const sql = `DELETE FROM ${tableName} WHERE ?`;
+    const result = await this.query(sql, condition);
+    return result;
   }
 }
