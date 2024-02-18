ca364967262c9d0a693d23be330650accd3d9feb
mahdi <mahdimamashli1383@gmail.com>
2024-02-18 15:13:34 +0330

    add manager dependencies

---
+++ src/manager/Dependencies.js
@@ -0,0 +1,48 @@
+const AppManager = require("./AppManager");
+
+function ensurePackage(packageName) {
+  try {
+    const requiredPackage = require(packageName);
+  } catch (e) {
+    throw new Error(
+      `The '${packageName}' module is not installed. Please make sure to install it by running 'npm install ${packageName}' before using sessions.`
+    );
+  }
+}
+
+class Dependencies extends AppManager {
+  constructor() {
+    super();
+  }
+  session(...options) {
+    if (!this.app) {
+      throw new Error("Express app has not been initialized yet.");
+    }
+    const session = ensurePackage("express-session");
+    /**@private */
+    this.s = session;
+    this.app.use(session(...options));
+  }
+  csrf() {
+    const csrf = ensurePackage("csurf");
+    const Protection = csrf();
+    this.use(Protection);
+  }
+  cors(...handler) {
+    const cors = ensurePackage("cors");
+    this.use(cors(...handler));
+  }
+  bodyParser(...handler) {
+    const bodyPater = ensurePackage("body-parser");
+    this.use(bodyPater(...handler));
+  }
+  flash() {
+    const flash = ensurePackage("connect-flash");
+    this.use(flash());
+  }
+  connectMongoDbSession(...options) {
+    const connectMongoDbSession = ensurePackage("connect-mongodb-session");
+    const store = new connectMongoDbSession(...options);
+    return store;
+  }
+}
