a5a1b94f875f4b74584f8fd8f16de4636e947e16
mahdi <mahdimamashli1383@gmail.com>
2024-02-19 12:23:19 +0330

    new test

--- tests/manager/AppManager.test.js
+++ tests/manager/AppManager.test.js
@@ -1,4 +1,5 @@
 const AppManager = require('../../src/manager/AppManager');
+
 describe('AppManager Class', () => {
   let appManager;
 
@@ -25,5 +26,39 @@ describe('AppManager Class', () => {
     // Ensure middleware is added to the Express app
     const middlewareCount = appManager.app._router.stack.length;
     expect(middlewareCount).toBeGreaterThan(0);
+  });
+
+  test('set sets properties on the Express application', () => {
+    const propertyName = 'title';
+    const propertyValue = 'My Express App';
+    appManager.set(propertyName, propertyValue);
+    // Ensure the property is set on the Express app
+    expect(appManager.app.get(propertyName)).toBe(propertyValue);
+  });
+
+  test('static serves static files and directories with Express', () => {
+    const directoryPath = 'public';
+    appManager.static(directoryPath);
+    // Ensure static files are served by Express
+    // You may need to inspect appManager.app._router.stack to check if static middleware is added
+    // and verify the correct directory is being served
+  });
+
+  test('useJsonBody enables JSON and URL-encoded parsing for request bodies', () => {
+    appManager.useJsonBody();
+    // Ensure JSON and URL-encoded parsing middleware are added to the Express app
+    // You may need to inspect appManager.app._router.stack to check if the middleware is added
+  });
+
+  test('shutdown shuts down the Express application', async () => {
+    // Mock the server close function
+    const closeMock = jest.fn((callback) => {
+      callback(); // Simulate successful server shutdown
+    });
+    appManager.app.close = closeMock;
+
+    await expect(appManager.shutdown()).resolves.toBeUndefined();
+    // Ensure app.close method is called during shutdown
+    expect(closeMock).toHaveBeenCalled();
   });
 });
