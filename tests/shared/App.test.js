928accdec171f652da84e0cb1fe21eab68273ec9
mahdi <mahdimamashli1383@gmail.com>
2024-02-19 11:05:30 +0330

    add new test

--- tests/shared/App.test.js
+++ tests/shared/App.test.js
@@ -1,23 +1,48 @@
-const App = require("../../src/shared/App"); // Require the App class
-const { initApp, listen, use } = new App(); // Destructure methods from the App instance
+const App = require("../../src/shared/App");
 
 describe("App", () => {
+  let app;
+
+  beforeEach(() => {
+    app = new App();
+  });
+
   test("initApp initializes the Express application", () => {
-    const app = initApp();
-    expect(app).toBeDefined(); // You're not accessing app.app directly, so just check if app is defined
+    const initializedApp = app.initApp();
+    expect(initializedApp).toBeDefined(); // Check if initializedApp is defined
     expect(app.runApp).toBe(true);
   });
 
   test("listen throws error if app is not initialized", () => {
     expect(() => {
-      listen();
+      app.listen();
     }).toThrowError("Express app has not been initialized yet.");
   });
 
   test("use adds middleware when app is initialized", () => {
-    initApp(); // Initialize the Express app first
+    app.initApp();
     const mockMiddleware = jest.fn();
-    use(mockMiddleware);
+    app.use(mockMiddleware);
     expect(mockMiddleware).toHaveBeenCalled();
+  });
+
+  test("launch initializes and starts the Express application", () => {
+    const port = 3000; // Set the port for testing
+    const textLog = `Server is running on port ${port}`;
+    const log = false; // Set log to false for testing
+
+    // Mock the console.log method
+    const originalConsoleLog = console.log;
+    console.log = jest.fn();
+
+    // Execute the launch method
+    app.launch(port, textLog, log);
+
+    // Expectations
+    expect(app.app).toBeDefined(); // Ensure app is defined
+    expect(app.runApp).toBe(true); // Ensure runApp is true
+
+    // Restore the original console.log method
+    console.log = originalConsoleLog;
   });
 });
