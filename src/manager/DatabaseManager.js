3ee33d0b887b58ada386a695e2d23ba7913aacc7
mahdi <mahdimamashli1383@gmail.com>
2024-02-19 14:36:29 +0330

    update

--- src/manager/DatabaseManager.js
+++ src/manager/DatabaseManager.js
@@ -1,37 +1,41 @@
-const MongoDb = require('../handler/database/MongoDB'), MySql = require('../handler/database/MySql')
+const MongoDb = require("../handler/database/MongoDB"),
+  MySql = require("../handler/database/MySql");
 function checkPkg(packageName) {
-    try {
-      const requiredPackage = require(packageName);
-      return requiredPackage;
-    } catch {throw new Error(`The '${packageName}' module is not installed. Please make sure to install it by running 'npm install ${packageName}' before using sessions.`);
-    }
+  try {
+    const requiredPackage = require(packageName);
+    return requiredPackage;
+  } catch {
+    throw new Error(
+      `The '${packageName}' module is not installed. Please make sure to install it by running 'npm install ${packageName}' before using sessions.`
+    );
   }
-  class DataBaseManager {
-    constructor() {}
-
-    /**
-     * Create and return a new instance of MySql.
-     * @returns {MySql} A new instance of MySql.
-     * @example
-     * const dbManager = new DataBaseManager();
-     * const mysql = dbManager.MySql();
-     */
-    MySql() {
-      const isMysql = 'mysql' ? 'mysql' : 'mysql2'
-        const pkg = checkPkg(isMysql);
-        return new MySql(pkg);
-    }
-
-    /**
-     * Create and return a new instance of MongoDb.
-     * @returns {MongoDb} A new instance of MongoDb.
-     * @example
-     * const dbManager = new DataBaseManager();
-     * const mongodb = dbManager.MongoDb();
-     */
-    MongoDb() {
-        const pkg = checkPkg('mongodb');
-        return new MongoDb(pkg);
-    }
 }
-module.exports = DataBaseManager
+class DataBaseManager {
+  constructor() {}
+
+  /**
+   * Create and return a new instance of MySql.
+   * @returns {MySql} A new instance of MySql.
+   * @example
+   * const dbManager = new DataBaseManager();
+   * const mysql = dbManager.MySql();
+   */
+  MySql() {
+    const isMysql = "mysql" ? "mysql" : "mysql2";
+    const pkg = checkPkg(isMysql);
+    return new MySql(pkg);
+  }
+
+  /**
+   * Create and return a new instance of MongoDb.
+   * @returns {MongoDb} A new instance of MongoDb.
+   * @example
+   * const dbManager = new DataBaseManager();
+   * const mongodb = dbManager.MongoDb();
+   */
+  MongoDb() {
+    const pkg = checkPkg("mongodb");
+    return new MongoDb(pkg);
+  }
+}
+module.exports = DataBaseManager;
