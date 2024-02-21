797f8b4e6969dda40ea8366eff09296432503bb7
mahdi <mahdimamashli1383@gmail.com>
2024-02-21 13:44:50 +0330

    Remove unused methods from `ResEnhancer`

--- src/handler/router/res/ResEnhancer.js
+++ src/handler/router/res/ResEnhancer.js
@@ -7,14 +7,6 @@ class ResEnhancer extends Response {
   constructor() {
     super();
   }
-  redirectWithCache(url, maxAge, isPrivate = false) {
-    this.cacheControl(maxAge, isPrivate);
-    this.redirect(url);
-  }
-  setCookieWithExpiration(name, value, expirationSeconds, options = {}) {
-    const expirationDate = new Date(Date.now() + expirationSeconds * 1000);
-    this.cookie(name, value, { expires: expirationDate, ...options });
-  }
   advCookie(){
     return new HandlerCookie(this.cookie)
   }
