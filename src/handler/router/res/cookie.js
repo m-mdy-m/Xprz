b12564209af4cd2742933a0a3e289039dbbd4786
mahdi <mahdimamashli1383@gmail.com>
2024-02-22 13:23:25 +0330

    add jsDoc

--- src/handler/router/res/cookie.js
+++ src/handler/router/res/cookie.js
@@ -1,26 +1,59 @@
+/**
+ * Class for handling cookies.
+ */
 class CookieHandler {
+  /**
+   * Creates an instance of CookieHandler.
+   * @param {string} [cookie=""] - The initial cookie string.
+   */
   constructor(cookie = "") {
     this.cookie = cookie;
   }
 
-  // Method to set a cookie
+  /**
+   * Sets a cookie with the given name, value, and options.
+   * @param {string} name - The name of the cookie.
+   * @param {string} value - The value of the cookie.
+   * @param {Object} [options={}] - Additional options for the cookie (e.g., expiration).
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * cookieHandler.setCookie('username', 'john_doe', { expires: 3600 });
+   */
   setCookie(name, value, options = {}) {
     const serializedCookie = this.serializeCookie(name, value, options);
     this.cookie = (this.cookie ? this.cookie + "; " : "") + serializedCookie;
   }
 
-  // Method to get a specific cookie by name
+  /**
+   * Retrieves the value of a specific cookie by name.
+   * @param {string} name - The name of the cookie to retrieve.
+   * @returns {string|undefined} The value of the cookie, or undefined if not found.
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * const username = cookieHandler.getCookie('username');
+   */
   getCookie(name) {
     const cookies = this.parseCookies();
     return cookies[name];
   }
-
-  // Method to get all cookies as an object
+  /**
+   * Retrieves all cookies as an object.
+   * @returns {Object} An object containing all cookies.
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * const allCookies = cookieHandler.getAllCookies();
+   */
   getAllCookies() {
     return this.parseCookies();
   }
-
-  // Method to remove a cookie
+  /**
+   * Removes a cookie with the given name.
+   * @param {string} name - The name of the cookie to remove.
+   * @param {Object} [options={}] - Additional options for the cookie (e.g., expiration).
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * cookieHandler.removeCookie('username');
+   */
   removeCookie(name, options = {}) {
     const cookies = this.parseCookies();
     delete cookies[name];
@@ -31,7 +64,10 @@ class CookieHandler {
       .join("; ");
   }
 
-  // Helper method to serialize a cookie
+  /**
+   * Helper method to serialize a cookie.
+   * @private
+   */
   serializeCookie(name, value, options = {}) {
     const encodedValue = encodeURIComponent(value);
     const parts = [`${name}=${encodedValue}`];
@@ -43,7 +79,10 @@ class CookieHandler {
     return parts.join("; ");
   }
 
-  // Helper method to parse cookies
+  /**
+   * Helper method to parse cookies.
+   * @private
+   */
   parseCookies() {
     const cookies = {};
     if (this.cookie) {
@@ -56,25 +95,39 @@ class CookieHandler {
     }
     return cookies;
   }
-
-  // Method to check if a cookie exists
+  /**
+   * Checks if a cookie with the given name exists.
+   * @param {string} name - The name of the cookie to check.
+   * @returns {boolean} True if the cookie exists, otherwise false.
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * const hasCookie = cookieHandler.hasCookie('username');
+   */
   hasCookie(name) {
     const cookies = this.parseCookies();
     return cookies.hasOwnProperty(name);
   }
 
-  // Method to clear all cookies
+  /**
+   * Clears all cookies.
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * cookieHandler.clearAllCookies();
+   */
   clearAllCookies() {
     this.cookie = "";
   }
-
-  // Method to get the number of cookies
+  /**
+   * Gets the number of cookies.
+   * @returns {number} The number of cookies.
+   * @example
+   * const cookieHandler = new CookieHandler();
+   * const cookieCount = cookieHandler.countCookies();
+   */
   countCookies() {
     const cookies = this.parseCookies();
     return Object.keys(cookies).filter((name) => cookies[name] !== "").length;
   }
 }
-
-module.exports = CookieHandler;
 
 module.exports = CookieHandler;
