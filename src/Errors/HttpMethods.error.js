e48291d7e95580469e697a114ce959c5289f1eb1
mahdi <mahdimamashli1383@gmail.com>
2024-02-22 16:06:18 +0330

    add comments

--- src/Errors/HttpMethods.error.js
+++ src/Errors/HttpMethods.error.js
@@ -1,20 +1,28 @@
+// HTTPMethodError class extends the built-in Error class to represent errors related to HTTP methods.
 class HTTPMethodError extends Error {
   constructor(message) {
+    // Call the constructor of the Error class with the provided message.
     super(message);
+    // Set the name of the error to the name of the constructor.
     this.name = this.constructor.name;
   }
 }
 
+// HTTPMethodRouteError class extends HTTPMethodError to represent errors that occur during route registration.
 class HTTPMethodRouteError extends HTTPMethodError {
   constructor(message) {
+    // Call the constructor of HTTPMethodError with a default message if no message is provided.
     super(message || "Error occurred during route registration.");
   }
 }
 
+// HTTPMethodExecutionError class extends HTTPMethodError to represent errors that occur during route execution.
 class HTTPMethodExecutionError extends HTTPMethodError {
   constructor(message) {
+    // Call the constructor of HTTPMethodError with a default message if no message is provided.
     super(message || "Error occurred during route execution.");
   }
 }
 
+// Export HTTPMethodRouteError and HTTPMethodExecutionError for use in other modules.
 module.exports = { HTTPMethodRouteError, HTTPMethodExecutionError };
--- src/Errors/package.manager.error.js
+++ src/Errors/package.manager.error.js
@@ -1,16 +1,24 @@
+// ModuleNotInstalledError class extends the built-in Error class to represent errors when required modules are not installed.
 class ModuleNotInstalledError extends Error {
   constructor(packageName) {
+    // Call the constructor of the Error class with a dynamically generated message.
     super(
       `The '${packageName}' module is not installed. Please make sure to install it by running 'npm install ${packageName}' before using sessions.`
     );
+    // Set the name of the error to the name of the constructor.
     this.name = this.constructor.name;
   }
 }
 
+// PackageInitializationError class extends Error to represent errors during package initialization.
 class PackageInitializationError extends Error {
   constructor(packageName, message) {
+    // Call the constructor of the Error class with a dynamically generated message.
     super(`Failed to initialize '${packageName}' package: ${message}`);
+    // Set the name of the error to the name of the constructor.
     this.name = this.constructor.name;
   }
 }
+
+// Export ModuleNotInstalledError and PackageInitializationError for use in other modules.
 module.exports = { ModuleNotInstalledError, PackageInitializationError };
