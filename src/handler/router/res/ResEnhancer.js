fb818fdb14e2a52f5399e0ef4d57d7c130342cf0
mahdi <mahdimamashli1383@gmail.com>
2024-02-22 12:27:44 +0330

    Adds advanced cookie, header, and JSON handlers to the ResEnhancer class.

--- src/handler/router/res/ResEnhancer.js
+++ src/handler/router/res/ResEnhancer.js
@@ -3,22 +3,47 @@ const JsonHandler = require("./Json");
 const CookieHandler = require("./cookie");
 const HeadersHandler = require("./header");
 
+/**
+ * ResEnhancer extends the base Response class to provide additional handlers for cookies, headers, and JSON.
+ * @class
+ */
 class ResEnhancer extends Response {
+  /**
+   * Creates a new ResEnhancer instance.
+   */
   constructor() {
     super();
   }
 
-  // Method to get an advanced cookie handler
+  /**
+   * Gets an advanced cookie handler.
+   * @returns {CookieHandler} Advanced cookie handler.
+   * @example
+   * const cookieHandler = resEnhancer.getCookieHandler();
+   * cookieHandler.set("myCookie", "cookieValue").send();
+   */
   getCookieHandler() {
     return new CookieHandler(this.cookie);
   }
 
-  // Method to get an advanced headers handler
+  /**
+   * Gets an advanced headers handler.
+   * @returns {HeadersHandler} Advanced headers handler.
+   * @example
+   * const headersHandler = resEnhancer.getHeadersHandler();
+   * headersHandler.set("Cache-Control", "no-cache").send();
+   */
   getHeadersHandler() {
     return new HeadersHandler(this.header, this.res);
   }
 
-  // Method to get an advanced JSON handler
+  /**
+   * Gets an advanced JSON handler.
+   * @returns {JsonHandler} Advanced JSON handler.
+   * @example
+   * const jsonHandler = resEnhancer.getJsonHandler();
+   * jsonHandler.send({ message: "Success" });
+   */
   getJsonHandler() {
     return new JsonHandler(this.json, this.status);
   }
