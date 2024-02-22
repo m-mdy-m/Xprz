b282994009761c66af1f5c13a27cf5e3a137094d
mahdi <mahdimamashli1383@gmail.com>
2024-02-22 19:53:27 +0330

    Adds comments and descriptions to the AppSharedManager class.

--- src/utils/shared.app.js
+++ src/utils/shared.app.js
@@ -1,12 +1,47 @@
 const { getApp, useApp, setApp, getExp } = require("../shareApp");
 
 class AppSharedManager {
-    constructor() {
-        this.getApp = getApp;
-        this.useApp = useApp;
-        this.setApp = setApp;
-        this.getExpress = getExp;
-    }
+  constructor() {
+    /**
+     * Gets the current Express application instance.
+     *
+     * @returns {Object|null} The Express application instance, or null if not initialized.
+     *
+     * @example
+     * const app = getApp();
+     * if (app) {
+     *   // Use the Express application instance
+     * } else {
+     *   throw new Error('Express app instance has not been initialized yet.');
+     * }
+     */
+    this.getApp = getApp;
+    /**
+     * Applies middleware to the current Express application instance.
+     *
+     * @param {...Function} handlers - Middleware functions to apply.
+     *
+     * @returns {Object} The modified Express application instance.
+     *
+     * @example
+     * const express = require('express');
+     * const app = express();
+     * useApp(middleware1, middleware2);
+     */
+    this.useApp = useApp;
+    /**
+     * Retrieves the Express module instance used within the application.
+     * @returns {Object|null} The Express module instance, or null if not set.
+     * @example
+     * const expressInstance = getExp();
+     * if (expressInstance) {
+     *   // Use the Express module instance
+     * } else {
+     *   throw new Error('Express module instance has not been initialized yet.');
+     * }
+     */
+    this.getExpress = getExp;
+  }
 }
 
 module.exports = AppSharedManager;
