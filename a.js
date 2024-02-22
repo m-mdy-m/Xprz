e03acdc84b63d5db882bb505ebf93d6e2aa2a3f0
mahdi <mahdimamashli1383@gmail.com>
2024-02-22 17:21:28 +0330

    Refactor code to use AppManager instead of App

--- a.js
+++ a.js
@@ -1,13 +1,10 @@
 const XPress = require('./index')
 
-const { App,AppManager,Database,HttpMethod,Package,Route,Utils,} = new XPress()
+const {AppManager,Database,HttpMethod,Package,Route,Utils,} = new XPress()
 
-const { closeServer,getExpress,initApp,launch,listen,use} = new App()
-launch()
-const { getApp} = Utils
-const app = getApp()
+const { launch} = new AppManager()
 const router = new Route()
 router.setRoute('/').get(()=>{
   const { send } =router.res()
   send('hi')
-}).attachTo(app)
+}).attachTo(launch())
--- index.js
+++ index.js
@@ -2,15 +2,13 @@ const AppManager = require("./src/manager/AppManager");
 const DatabaseManager = require("./src/manager/DatabaseManager");
 const PackageManager = require("./src/manager/PackageManager");
 const { getApp, useApp, setApp,getExp } = require("./src/shareApp");
-const BaseApp = require("./src/shared/App");
 const httpMethod = require("./src/shared/HTTPMethod");
 const RouteManager = require("./src/shared/RouteManager");
 
 class XPress {
   constructor() {
     this.Utils = { getApp, useApp, setApp,getExp };
-    this.App = BaseApp;
-    this.AppManager = AppManager;
+    this.App = AppManager;
     this.Database = DatabaseManager;
     this.Package = PackageManager;
     this.HttpMethod = httpMethod;
