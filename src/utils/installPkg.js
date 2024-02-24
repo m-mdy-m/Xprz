a022348cf9f423c735757c6c4517752e165c8a04
mahdi <mahdimamashli1383@gmail.com>
2024-02-24 16:18:17 +0330

    Update `installPkg.js` to retry package installation and require.

--- src/utils/installPkg.js
+++ src/utils/installPkg.js
@@ -4,11 +4,26 @@ const {
   } = require("../Errors/package.manager.error"),
   { execSync } = require("child_process");
 
-function _checkPkg(packageName) {
-  try {
-    return require(packageName);
-  } catch {
-    return null;
+async function _checkPkg(packageName, retries = 10, delay = 100) {
+  while (retries > 0) {
+    try {
+      // Clear the cache for the package
+      delete require.cache[require.resolve(packageName)];
+      // Require the package
+      const pkg = require(packageName);
+      // If the package is successfully required, return it
+      return pkg;
+    } catch (error) {
+      // If an error occurs, decrement the number of retries and wait before trying again
+      retries--;
+      if (retries === 0) {
+        // If no more retries left, return null
+        return null;
+      }
+      // Wait for the specified delay before retrying
+      console.log(`Retrying in ${delay / 1000} seconds...`);
+      await new Promise((resolve) => setTimeout(resolve, delay));
+    }
   }
 }
 function $install(package) {
@@ -16,13 +31,17 @@ function $install(package) {
     let isPkg = _checkPkg(package);
     if (!isPkg) {
       execSync(`npm install ${package}`);
-      isPkg = _checkPkg(package); // Update isPkg after installation
+      // Check the package after installation
+      isPkg = _checkPkg(package);
     }
     return isPkg;
   } catch (er) {
     throw new PackageInitializationError(package, er.message);
   }
 }
-const a = $install("nodemailer");
-console.log("a =>", a);
+const run = async () => {
+  const a = await $install("nodemailer");
+  console.log("a =>", a);
+};
+run();
 module.exports = $install;
