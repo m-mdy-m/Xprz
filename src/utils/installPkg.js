5e457406d7ab8a79785318a06ba51f6edff4b462
mahdi <mahdimamashli1383@gmail.com>
2024-02-24 16:55:15 +0330

    Install package using `npm install` if it's not available.

--- src/utils/installPkg.js
+++ src/utils/installPkg.js
@@ -9,34 +9,32 @@ const sleep = promisify(setTimeout);
 function _checkPkg(packageName, retries = 10, delay = 100) {
   while (retries > 0) {
     try {
-      // Require the package
       const pkg = require(packageName);
-      // If the package is successfully required, return it
       return pkg;
     } catch (error) {
-      // If an error occurs, decrement the number of retries and wait before trying again
       retries--;
       if (retries === 0) {
-        // If no more retries left, return null
         return null;
       }
-      sleep(delay);
+      // Synchronous delay using setTimeout
+      setTimeout(() => {}, delay);
     }
   }
 }
 
 function $install(package) {
   try {
-    // Install the package
-    execSync(`npm install ${package}`);
-    // Check the package after installation
-    const isPkg = _checkPkg(package);
-    return isPkg;
+    let isPkg = _checkPkg(package);
+    if (!isPkg) {
+      execSync(`npm install ${package}`);
+      isPkg = _checkPkg(package);
+    }
+    return isPkg
   } catch (er) {
     throw new PackageInitializationError(package, er.message);
   }
 }
 
-const a = $install("nodemailer");
+const a = $install("vfyjs");
 console.log("a =>", a);
 module.exports = $install;
