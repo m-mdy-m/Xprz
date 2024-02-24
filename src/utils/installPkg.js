941bad1eaf88d707d0fabf6d12aae0999670f5a7
mahdi <mahdimamashli1383@gmail.com>
2024-02-24 16:32:33 +0330

    Update package.json and installPkg.js
    - Added nodemailer as a dependency in package.json.
    - Updated installPkg.js to use async/await and promisify for better error handling and code readability.
    - Added a synchronous version of _checkPkg for cases where synchronous behavior is preferred

--- src/utils/installPkg.js
+++ src/utils/installPkg.js
@@ -1,41 +1,55 @@
 const { execSync } = require("child_process");
 const {
-  PackageInitializationError,
-} = require("../Errors/package.manager.error");
+    PackageInitializationError,
+  } = require("../Errors/package.manager.error"),
+  { promisify } = require("util");
 
-function _checkPkg(packageName, retries = 10, delay = 100) {
+const sleep = promisify(setTimeout);
+
+async function _checkPkgAsync(packageName, retries = 10, delay = 100) {
   while (retries > 0) {
     try {
-      // Require the package
       const pkg = require(packageName);
-      // If the package is successfully required, return it
       return pkg;
     } catch (error) {
-      // If an error occurs, decrement the number of retries and wait before trying again
       retries--;
       if (retries === 0) {
-        // If no more retries left, return null
         return null;
       }
-      // Synchronous delay using setTimeout
-      const start = Date.now();
-      while (Date.now() - start < delay) {}
+      await sleep(delay);
     }
   }
 }
 
+function _checkPkgSync(packageName, retries = 10, delay = 100) {
+  let result = null;
+  while (retries > 0) {
+    try {
+      result = require(packageName);
+      break;
+    } catch (error) {
+      retries--;
+      if (retries === 0) {
+        break;
+      }
+      sleep(delay);
+    }
+  }
+  return result;
+}
+
 function $install(package) {
   try {
-    // Install the package
-    execSync(`npm install ${package}`);
-    // Check the package after installation
-    const isPkg = _checkPkg(package);
+    let isPkg = _checkPkgSync(package);
+    if (!isPkg) {
+      execSync(`npm install ${package}`);
+      isPkg = _checkPkgAsync(package);
+    }
     return isPkg;
   } catch (er) {
     throw new PackageInitializationError(package, er.message);
   }
 }
-
 const a = $install("nodemailer");
 console.log("a =>", a);
 module.exports = $install;
