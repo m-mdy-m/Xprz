7a592c1be93b47b460fd5d0844a4412df8b1632c
mahdi <mahdimamashli1383@gmail.com>
2024-02-24 16:35:26 +0330

    Update `installPkg.js` to use `execSync` instead of `require` to check if a package is installed.

--- src/utils/installPkg.js
+++ src/utils/installPkg.js
@@ -6,50 +6,37 @@ const {
 
 const sleep = promisify(setTimeout);
 
-async function _checkPkgAsync(packageName, retries = 10, delay = 100) {
+function _checkPkg(packageName, retries = 10, delay = 100) {
   while (retries > 0) {
     try {
+      // Require the package
       const pkg = require(packageName);
+      // If the package is successfully required, return it
       return pkg;
     } catch (error) {
+      // If an error occurs, decrement the number of retries and wait before trying again
       retries--;
       if (retries === 0) {
+        // If no more retries left, return null
         return null;
-      }
-      await sleep(delay);
-    }
-  }
-}
-
-function _checkPkgSync(packageName, retries = 10, delay = 100) {
-  let result = null;
-  while (retries > 0) {
-    try {
-      result = require(packageName);
-      break;
-    } catch (error) {
-      retries--;
-      if (retries === 0) {
-        break;
       }
       sleep(delay);
     }
   }
-  return result;
 }
 
 function $install(package) {
   try {
-    let isPkg = _checkPkgSync(package);
-    if (!isPkg) {
-      execSync(`npm install ${package}`);
-      isPkg = _checkPkgAsync(package);
-    }
+    // Install the package
+    execSync(`npm install ${package}`);
+    // Check the package after installation
+    const isPkg = _checkPkg(package);
     return isPkg;
   } catch (er) {
     throw new PackageInitializationError(package, er.message);
   }
 }
+
 const a = $install("nodemailer");
 console.log("a =>", a);
 module.exports = $install;
