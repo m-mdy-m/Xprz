7e51f7da11fe35e2d710a1f40c445c123d96c91e
mahdi <mahdimamashli1383@gmail.com>
2024-02-25 20:08:12 +0330

    Update template engine setup functions

--- src/utils/templateEngines.js
+++ src/utils/templateEngines.js
@@ -1,49 +1,42 @@
 // Import the `getApp` function from the "../Using" module
 const { getApp } = require("../shareApp");
 const $install = require("./installPkg");
-const $read = require("./read");
-
 let app = getApp();
 if (!app) {
-  return app
-}else{
+  return app;
+} else {
+  function setEjs(dir = "views") {
+    $install("ejs");
+    // Set the view engine to EJS
+    app.set("view engine", "ejs");
+    // Set the views directory, defaulting to "views" if dir is not provided
+    app.set("views", dir);
+  }
+  function setHBS(hbs, dir, options = {}) {
+    // Default options for Handlebars
+    const defaultOptions = {
+      defaultLayout: "main",
+      layoutsDir: `${dir}/layouts`, // Corrected line
+      partialsDir: `${dir}/partials`, // Corrected line
+      extname: ".hbs",
+    };
 
-function setEjs(dir="views") {
-  $install('ejs')
-  // Set the view engine to EJS
-  app.set("view engine", "ejs");
-  // Set the views directory, defaulting to "views" if dir is not provided
-  app.set("views", dir);
-}
+    // Combine the default options with any provided options
+    const combinedOptions = { ...defaultOptions, ...options };
 
-
-function setHBS(hbs, dir, options = {}) {
-  // Default options for Handlebars
-  const defaultOptions = {
-    defaultLayout: "main",
-    layoutsDir: `${dir}/layouts`, // Corrected line
-    partialsDir: `${dir}/partials`, // Corrected line
-    extname: ".hbs",
-  };
-
-  // Combine the default options with any provided options
-  const combinedOptions = { ...defaultOptions, ...options };
-
-  // Set the view engine to Handlebars
-  app.set("view engine", "hbs");
-  // Set the views directory
-  app.set("views", dir);
-  // Configure Handlebars engine with the combined options
-  app.engine(".hbs", hbs(combinedOptions));
-}
-
-
-function setPug(dir="views") {
-  // Set the view engine to Pug
-  app.set("view engine", "pug");
-  // Set the views directory, defaulting to "views" if dir is not provided
-  app.set("views", dir);
-}
-// Export the functions to be used elsewhere
-module.exports = {setEjs,setHBS,setPug}
+    // Set the view engine to Handlebars
+    app.set("view engine", "hbs");
+    // Set the views directory
+    app.set("views", dir);
+    // Configure Handlebars engine with the combined options
+    app.engine(".hbs", hbs(combinedOptions));
+  }
+  function setPug(dir = "views") {
+    // Set the view engine to Pug
+    app.set("view engine", "pug");
+    // Set the views directory, defaulting to "views" if dir is not provided
+    app.set("views", dir);
+  }
+  // Export the functions to be used elsewhere
+  module.exports = { setEjs, setHBS, setPug };
 }
