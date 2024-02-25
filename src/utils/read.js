43bb26c22a64faca2b1d60a4c7180d0bd62c156c
mahdi <mahdimamashli1383@gmail.com>
2024-02-25 10:04:18 +0330

    Resolve path by handling '../' in the location

--- src/utils/read.js
+++ src/utils/read.js
@@ -7,12 +7,27 @@ const path = require("path");
  * @private
  */
 function _resolvePath(location) {
-  // Resolve the absolute path
-  return path.resolve(process.cwd(), location);
+    // Split the location by directory separator
+    const parts = location.split('/');
+  
+    // Iterate through the parts to handle '../'
+    const resolvedParts = [];
+    for (const part of parts) {
+      if (part === '..') {
+        // If part is '..', pop the last resolved part
+        resolvedParts.pop();
+      } else {
+        // Otherwise, add the part to the resolved parts
+        resolvedParts.push(part);
+      }
+    }
+    
+    // Resolve the absolute path
+    return path.resolve(process.cwd(), ...resolvedParts);
 }
 
 /**
- * Loads a file or folder using require.
+ * Loads a file or folder using require.a
  * @param {string} location - The path of the file or folder to load.
  * @returns {object} - The loaded module.
  * @throws {Error} If the file or folder does not exist or cannot be required.
