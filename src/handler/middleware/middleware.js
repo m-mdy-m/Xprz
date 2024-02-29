493d19ac12e671b6551ad7b1d21ec2e43a76e32d
mahdi <mahdimamashli1383@gmail.com>
2024-02-29 18:55:56 +0330

    add middleware tests

---
+++ src/handler/middleware/middleware.js
@@ -0,0 +1,8 @@
+function ensureAuthenticated(statuscode = 401, redirect = "/signup") {
+  return (req, res, nxt) => {
+    if (req.session && req.session.user) {
+      return nxt();
+    }
+    return res.status(statuscode).redirect(redirect);
+  };
+}
