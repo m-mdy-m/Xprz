b482cec75cc81c68cf7290c479066786aeac593b
mahdi <mahdimamashli1383@gmail.com>
2024-03-01 13:56:05 +0330

    Delete ensureAuthenticated middleware function.

--- src/handler/middleware/middleware.js
+++
@@ -1,8 +0,0 @@
-function ensureAuthenticated(statuscode = 401, redirect = "/signup") {
-  return (req, res, nxt) => {
-    if (req.session && req.session.user) {
-      return nxt();
-    }
-    return res.status(statuscode).redirect(redirect);
-  };
-}
