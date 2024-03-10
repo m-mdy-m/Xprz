62fcce5e3b7724b5f4b21530c0df939d9f967f60
mahdi <mahdimamashli1383@gmail.com>
2024-03-11 00:42:50 +0330

    Changed `app` to `app()` in `CsrfHandler` class.

--- src/handler/package/csrf.js
+++ src/handler/package/csrf.js
@@ -11,7 +11,7 @@ class CsrfHandler {
     /** @private */
     this.csrf = csrf;
     /** @private */
-    this.app = app;
+    this.app = app();
     /** @private */
     this.use = use;
     /** @private */
