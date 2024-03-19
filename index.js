0931134260bef12f53e832983a91482b45dc6772
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 00:45:47 +0330

    Expose `$install` and `$read` globally.

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+// Import necessary modules
 const AppManager = require("./src/manager/AppManager");
 const DatabaseManager = require("./src/manager/DatabaseManager");
 const PackageManager = require("./src/manager/PackageManager");
 const AppSharedManager = require("./src/utils/shared.app");
 const httpMethod = require("./src/shared/HTTPMethod");
 const RouteManager = require("./src/shared/RouteManager");
-const $install = require("./src/utils/installPkg"),$read = require('./src/utils/read')
+const $install = require("./src/utils/installPkg");
+const $read = require("./src/utils/read");
+
+/**
+ * Represents the main application class.
+ * @m-mdy-m
+ * @package
+ */
 class Xprz {
+  /**
+   * Constructs a new instance of the Xprz application.
+   */
   constructor() {
+    // Expose shared modules
     this.SharedApp = AppSharedManager;
+
+    // Initialize managers
     this.App = AppManager;
     this.Database = DatabaseManager;
     this.Package = PackageManager;
+
+    // Expose HTTP methods
     this.HttpMethod = httpMethod;
+
+    // Expose route manager
     this.Route = RouteManager;
   }
 }
-// Set $install globally
+
+// Expose $install and $read globally
 global.$install = $install;
-global.$read = $read
+global.$read = $read;
+
+// Export the Xprz class
 module.exports = Xprz;
