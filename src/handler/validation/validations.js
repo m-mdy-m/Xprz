b4eb5dfd65f8b847aee9a2130f59e5a4fa0e41a4
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 00:16:23 +0330

    Add validation for request body using vfyjs library

--- src/handler/validation/validations.js
+++ src/handler/validation/validations.js
@@ -1,15 +1,45 @@
 // validation.js
 const { RequestValidator } = require("vfyjs");
-
+/**
+ * A class for validating request data using the vfyjs library.
+ */
 class Validation {
-  constructor(request) {
-    this._req = request;
-    this._validator = new RequestValidator();
+    /**
+     * Create a Validation instance.
+     * @param {object} request - The request object containing data to be validated.
+     */
+    constructor(request) {
+      /**
+       * The request object.
+       * @type {object}
+       * @private
+       */
+      this._req = request;
+  
+      /**
+       * The RequestValidator instance.
+       * @type {RequestValidator}
+       * @private
+       */
+      this._validator = new RequestValidator();
+    }
+  
+    /**
+     * Validates the body of the request against the provided rules.
+     * @param {object} rules - The validation rules to be applied.
+     * @param {object} [options={}] - Additional options for validation.
+     * @returns {object} - The validation result.
+     * @example
+     * const validationRules = {
+     *   username: 'string',
+     *   password: 'string|min:6',
+     * };
+     * const validationResult = validateBody(validationRules);
+     */
+    body(rules, options = {}) {
+      return this._validator.validate(this._req.body, rules, options);
+    }
   }
-
-  body(rules, options = {}) {
-    return this._validator.validate(this._req.body, rules, options);
-  }
-}
-
-module.exports = Validation;
+  
+  module.exports = Validation;
+  
