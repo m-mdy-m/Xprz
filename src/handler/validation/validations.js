cd255671d59846818b7f645037c519016b2aef6a
mahdi <mahdimamashli1383@gmail.com>
2024-03-19 23:59:24 +0330

    Refactor validation code using vfyjs library

--- src/handler/validation/validations.js
+++ src/handler/validation/validations.js
@@ -1,26 +1,14 @@
-const { RequestValidator } = require('vfyjs');
+const { RequestValidator } = require("vfyjs");
 
 class Validation {
-    constructor(request) {
-        this._req = request;
-        this._validator = new RequestValidator();
-    }
+  constructor(request) {
+    this._req = request;
+    this._validator = new RequestValidator();
+  }
 
-    _validate(data, rules, options = {}) {
-        return this._validator.validate(data, rules, options);
-    }
-
-    body(rules, options = {}) {
-        return this._validate(this._req.body, rules, options);
-    }
-
-    query(rules, options = {}) {
-        return this._validate(this._req.query, rules, options);
-    }
-
-    param(rules, options = {}) {
-        return this._validate(this._req.param, rules, options);
-    }
+  _validate(data, rules, options = {}) {
+    return this._validator.validate(data, rules, options);
+  }
 }
 
 module.exports = Validation;
