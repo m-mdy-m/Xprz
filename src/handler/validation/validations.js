2745b04dffc3b7967df6b36c62f6af7040f4f2b1
mahdi <mahdimamashli1383@gmail.com>
2024-03-20 00:00:15 +0330

    Add a class for validating request data using vfyjs library.

--- src/handler/validation/validations.js
+++ src/handler/validation/validations.js
@@ -1,14 +1,41 @@
 const { RequestValidator } = require("vfyjs");
 
+/**
+ * A class for validating request data using vfyjs library.
+ */
 class Validation {
-  constructor(request) {
-    this._req = request;
-    this._validator = new RequestValidator();
+    /**
+     * Create a Validation instance.
+     * @param {object} request - The request object containing data to be validated.
+     */
+    constructor(request) {
+      /**
+       * The request object.
+       * @type {object}
+       * @private
+       */
+      this._req = request;
+      
+      /**
+       * The RequestValidator instance.
+       * @type {RequestValidator}
+       * @private
+       */
+      this._validator = new RequestValidator();
+    }
+  
+    /**
+     * Validates the provided data against the given rules.
+     * @param {object} data - The data to be validated.
+     * @param {object} rules - The validation rules.
+     * @param {object} [options={}] - Additional options for validation.
+     * @returns {object} - The validation result.
+     * @private
+     */
+    _validate(data, rules, options = {}) {
+      return this._validator.validate(data, rules, options);
+    }
   }
-
-  _validate(data, rules, options = {}) {
-    return this._validator.validate(data, rules, options);
-  }
-}
-
-module.exports = Validation;
+  
+  module.exports = Validation;
+  
