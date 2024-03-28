b97f58ac65b4c35bd4efb24bb7baf4af1a7c001b
mahdi <mahdimamashli1383@gmail.com>
2024-03-28 04:55:16 +0330

    Update Xprz framework example with minor changes to imports and module requirements
    - Change from `const Xprz = require("xprz");` to `const Xprz = require('xprz')`
    - Update destructuring of required functions from Xprz App module
    - Remove unnecessary imports and update comments accordingly

--- example/app.js
+++ example/app.js
@@ -1,20 +1,10 @@
 /**
  * Example setup for using the Xprz framework.
  */
-const Xprz = require("xprz");
+const Xprz = require('xprz')
 
 // Destructure required functions from Xprz App module
-const {
-  use,
-  launch,
-  loadRoutes,
-  useJsonBody,
-  static,
-  initApp,
-  listen,
-  closeServer,
-  setViewEngine,
-} = Xprz.App();
+const { use, launch, loadRoutes, useJsonBody, static,initApp,listen, closeServer,setViewEngine} = Xprz.App();
 
 // Launch the server
 launch();
@@ -22,19 +12,20 @@ launch();
 // initApp();
 // listen();
 
+
 // Set the view engine to 'ejs'
-setViewEngine("ejs");
+setViewEngine('ejs');
 // Enable JSON body parsing
 useJsonBody();
 // Serve static files from the 'public' directory
-static("public");
+static('public');
 // Example usage of middleware 'cors' using $install
-const cors = $install("cors");
+const cors = $install('cors');
 use(cors());
 // Load routes from the 'routes' directory automatically
-loadRoutes("routes");
+loadRoutes('routes');
 
 // Gracefully close the server
 closeServer(() => {
-  console.log("Server closed");
+    console.log('Server closed');
 });
